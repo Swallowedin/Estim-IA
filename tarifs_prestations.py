bb2eb5ecc8d0ad59747750f2919a7d7a249d8b7e
Swallowedin <charles.thomasse@gmail.com>
2024-10-09 14:58:17 +0200

    Update tarifs_prestations.py

--- tarifs_prestations.py
+++ tarifs_prestations.py
@@ -1,5 +1,6 @@
 def get_tarifs():
-    return {
+ return {
+    "prestations": {
         "tarif_horaire_standard": 250,  # Taux horaire moyen
         "tarif_externalisation": 150,  # Taux horaire moyen
         "facteur_urgence": 1.5,  # Facteur multiplicateur pour les cas urgents
